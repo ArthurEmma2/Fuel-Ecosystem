310e79fd43589b37615700304a2461570fe63259
Arthur Emmanuel <yhawnoworries@gmail.com>
2023-02-14 16:03:31 +0000

    update

---
+++ src/Assets/data/Articles.js
@@ -0,0 +1,58 @@
+export const article = [
+    
+    {
+    id:  1,
+    title: "Introducing Fuel - The Fastest Modular Execution Layer",
+    img: "",
+    url: "https://fuel-labs.ghost.io/introducing-fuel-the-fastest-modular-execution-layer/"
+    },
+
+    {
+        id:  2,
+        title: "Exploring the FuelVM: Next generation, scalable, blockchain VMs are here",
+        img: "",
+        url: "https://medium.com/blockchain-capital-blog/exploring-the-fuelvm-86cf9ccdc159"
+        },
+
+        {
+            id:  3,
+            title: "Is Fuel The Best Modular Execution Layer?",
+            img: "",
+            url: "https://members.delphidigital.io/reports/is-fuel-the-best-modular-execution-layer#conclusion"
+            },
+
+            {
+                id:  4,
+                title: "The Fuel for Fast Execution",
+                img: "",
+                url: "https://maven11.substack.com/p/the-fuel-for-fast-execution"
+                },
+
+                {
+                    id:  5,
+                    title: "Fuel Labs Announces $80 Million of Support for the Fastest Modular Execution Layer",
+                    img: "",
+                    url: "https://fuel-labs.ghost.io/fuel-labs-announces-80-million-of-support-for-the-fastest-modular-execution-layer/"
+                    },
+
+                    {
+                        id:  6,
+                        title: "Introducing Beta-1, the First Modular Execution Layer Developer-facing Testnet",
+                        img: "",
+                        url: ""
+                        },
+
+                        {
+                            id:  7,
+                            title: "Announcing “Beta-2”: Fuel's Second Public Testnet",
+                            img: "",
+                            url: "https://fuel-labs.ghost.io/announcing-beta-2-testnet/"
+                            },
+
+                            {
+                                id:  8,
+                                title: "How to deploy your own token on Fuel Testnet using Rust SDK?",
+                                img: "",
+                                url: "https://medium.com/@swaygang/how-to-deploy-your-own-token-on-fuel-testnet-using-rust-sdk-99b48a977e19"
+                                },
+]
