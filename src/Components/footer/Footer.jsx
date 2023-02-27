fe0dec72102eb94a549b111aa54d4ed87a75eede
Arthur Emmanuel <yhawnoworries@gmail.com>
2023-02-27 21:26:58 +0000

    update

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <section className='bg-black h-24'>
-        <div className='container flex justify-between mx-5'>
-            <p className='text-white'>Github</p> <i class="fa fa-github" aria-hidden="true"></i>
-            <p className = "text-white">copyright</p>
+    <section className='bg-gray-700 h-20'>
+        <div className='container flex justify-between mx-5 pt-8 text-white'>
+            <a target="_blank" rel="noreferrer" href = "https://github.com/ArthurEmma2/Fuel-Ecosystem">
+            <p className=' cursor-pointer text-xl'>Github <i class="ri-github-fill"></i></p> <i class="fa fa-github" aria-hidden="true"></i></a>
+            <p className = "text-sm">copyright 2023 ...</p>
         </div>
     </section>
   )
