6c5ebee424757c74140903ca4be112628c353050
Arthur Emmanuel <yhawnoworries@gmail.com>
2023-02-28 08:38:48 +0000

    update

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <section className='bg-gray-700 h-20 w-full overflow-x-hidden '>
-        <div className='container flex justify-around md:justify-between mx-5 pt-8 text-white items-center'>
-            <a target="_blank" rel="noreferrer" href = "https://github.com/ArthurEmma2/Fuel-Ecosystem">
-            <p className=' cursor-pointer text-xl'>Github <i class="ri-github-fill"></i></p> <i class="fa fa-github" aria-hidden="true"></i></a>
-            <p className = "text-sm">copyright 2023</p>
-        </div>
+    <section className="bg-gray-700 h-20 w-full overflow-x-hidden mt-24">
+      <div className="container flex justify-around md:justify-between mx-5 pt-8 text-white items-center">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/ArthurEmma2/Fuel-Ecosystem"
+        >
+          <p className=" cursor-pointer text-xl">
+            Github <i class="ri-github-fill"></i>
+          </p>{" "}
+          <i class="fa fa-github" aria-hidden="true"></i>
+        </a>
+        <p className="text-sm">copyright 2023</p>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
